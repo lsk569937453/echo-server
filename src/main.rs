3c7a2ec83a9e40cfe324786d226d79a8fb99d15e
shikai liu <lsk569937453@gmail.com>
2024-05-29 15:58:58 +0800

    Update the help message.

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,16 @@ extern crate tracing;
 #[command(author, version, about, long_about)]
 struct Cli {
     /// The http port,default port is 80
-    #[arg(default_value_t = 80, short = 'P', long = "port", value_name = "port")]
+    #[arg(default_value_t = 80, short = 'P', long = "port", value_name = "Port")]
     http_port: u32,
-    #[arg(default_value_t = 9898)]
+    /// The grpc port,default port is 8989
+
+    #[arg(
+        default_value_t = 8989,
+        short = 'G',
+        long = "grpc_port",
+        value_name = "Grpc Port"
+    )]
     grpc_port: u32,
 }
 
